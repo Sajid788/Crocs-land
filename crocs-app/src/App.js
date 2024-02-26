2b00cb867621fed961dffd35207688c90f388899
Md Sajid Ansari <mdsajidalam8340@gmail.com>
2024-02-26 13:44:17 +0530

    Fotter done

--- crocs-app/src/App.js
+++ crocs-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Home from './Page/Home';
+import Footer from "./Componant/Footer";
 
 function App() {
   return (
     <div className="App">
       <Home/>
+      <Footer/>
     </div>
   );
 }
